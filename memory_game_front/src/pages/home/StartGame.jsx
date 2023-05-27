10951c8374c149c2bb4eacf1fca7c89fc2b2186f
Shoug <92891184+alsuhaibanishoug@users.noreply.github.com>
2023-05-27 12:13:20 +0300

    Update
    add sign out button in home page

--- memory_game_front/src/pages/home/StartGame.jsx
+++ memory_game_front/src/pages/home/StartGame.jsx
@@ -21,8 +21,13 @@ const StartGame = () => {
                 <div className="d-flex justify-content-between align-items-center">
                     <div className="main_title">memory</div>
                     {/* <div className="signin">Sign in</div> */}
+                    <div className="d-flex justify-content-between align-items-center gap-2" >
                     <CusButton cusClass={'signin__btn'} callBackFn={goToProfile}><span>Account</span></CusButton>
-
+                    <CusButton cusClass={'signin__btn'} callBackFn={()=>{
+                        localStorage.clear();
+                        navigate('/',{replace: true});
+                    }}><span>Log Out</span></CusButton>
+                    </div>
                 </div >
                 <div className="bTxt">
                     <span>Uncover to matching cardsat once to</span> <br />
